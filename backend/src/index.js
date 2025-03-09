8eb43ba3d9e7d04485c26035145993f1c5eb6951
JishnuSharma <jishnusharma18@gmail.com>
2025-03-10 00:17:26 +0530

    Initial setup for message functionality backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
@@ -14,6 +15,8 @@ app.use(cookieParser());
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
 
 app.listen(PORT,() => {
     console.log("The app is listening on port:" + PORT);
---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { protectedRoute } from "../middlewares/auth.middleware.js";
+import { getInContactUsers, getMessages, sendMessage } from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+router.get("/users", protectedRoute, getInContactUsers);
+router.get("/:id",protectedRoute,getMessages);
+router.post("/send/:id",protectedRoute,sendMessage);
+
+
+export default router;
